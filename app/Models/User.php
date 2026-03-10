404a86eb2ad3c5ac0941ceead5494828f25fc099
Nirjala Shakya <nirjala.shakya@amniltech.com>
2026-03-10 07:03:33 +0545

    feat: modify user table, model and factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,14 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'phone',
+        'dob',
+        'gender',
+        'address',
     ];
 
     /**
@@ -30,7 +35,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -41,7 +45,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,22 +23,27 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'phone' => $this->faker->phoneNumber(),
+            'dob' => $this->faker->date(),
+            'gender' => $this->faker->randomElement(['m', 'f', 'o']),
+            'address' => $this->faker->address(),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
+    // public function unverified(): static
+    // {
+    //     return $this->state(fn (array $attributes) => [
+    //         'email_verified_at' => null,
+    //     ]);
+    // }
 }
