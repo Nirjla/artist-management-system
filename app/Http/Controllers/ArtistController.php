cae3cdd70a64b16a55712daf64dc861514532895
Nirjala Shakya <nirjala.shakya@amniltech.com>
2026-03-11 06:24:56 +0545

    fix: typo in validation

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -47,7 +47,7 @@ class ArtistController extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
-            'name' => 'required|string:maax:150',
+            'name' => 'required|string:max:150',
             'dob' => 'required|date',
             'gender' => 'required|in:m,f,o',
             'address' => 'required|string',
