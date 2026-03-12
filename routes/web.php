4319b3af95c9b748bc0086de0fdd88daf98e5120
nirjla <shakyanirjala6@gmail.com>
2026-03-12 07:18:18 +0545

    feat:  add the routes to handle users and artists

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -13,8 +14,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::post('/user', [UserController::class, 'store'])->name('user.store');
+    Route::patch('/user/{id}', [UserController::class, 'update'])->name('user.update');
+    Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -22,6 +26,8 @@ Route::middleware('auth')->group(function () {
     Route::post('/artist', [ArtistController::class, 'store'])->name('artist.store');
     Route::put('/artist/{id}', [ArtistController::class, 'update'])->name('artist.update');
     Route::delete('/artist/{id}', [ArtistController::class, 'destroy'])->name('artist.destroy');
+    Route::get('/artist/export', [ArtistController::class, 'export'])->name('artist.export');
+    Route::get('/artists/import', [ArtistController::class, 'import'])->name('artist.import');
 });
 
 require __DIR__ . '/auth.php';
